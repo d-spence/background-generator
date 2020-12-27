4ee9df5d926ba510a454c3a04e308273d4d8eecf
Dan Spencer <dspence180@gmail.com>
2020-12-27 13:34:11 -0600

    Added a button to change the gradient angle/direction in 45deg increments

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-var css = document.querySelector("h3");
+var css = document.querySelector("#gradientcss");
 var color1 = document.querySelector("#color1");
 var color2 = document.querySelector("#color2");
 var random = document.querySelector("#random");
-var body = document.getElementById("gradient");
+var angle = document.querySelector("#angle");
+var body = document.querySelector("#gradient");
 
 // console.log(body);
 // console.log(color1);
 // console.log(color2);
+
+const ANGLES = ['0deg', '45deg', '90deg', '135deg', '180deg', '225deg', '270deg', '315deg'];
+let currentAngleIndex = 0;
 
 function setGradient() {
     body.style.background = getGradientCSS();
@@ -15,7 +19,7 @@ function setGradient() {
 
 function getGradientCSS() {
     // Return the linear gradient css property of current bg
-    return "linear-gradient(to right, "+color1.value+", "+ color2.value+")";
+    return `linear-gradient(${ANGLES[currentAngleIndex]}, ${color1.value}, ${color2.value})`;
 }
 
 function randomColor() {
@@ -26,14 +30,27 @@ function randomColor() {
 }
 
 function randomGradient() {
+    // Set random color values and apply css
     color1.value = randomColor();
     color2.value = randomColor();
+    setGradient();
+}
+
+function nextAngle() {
+    // Return the next angle from ANGLES array
+    if (currentAngleIndex === ANGLES.length-1) {
+        currentAngleIndex = 0;
+    } else {
+        currentAngleIndex++;
+    }
+    console.log(currentAngleIndex, ANGLES[currentAngleIndex]);
     setGradient();
 }
 
 color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
 random.addEventListener("click", randomGradient);
+angle.addEventListener("click", nextAngle);
 
 document.addEventListener('DOMContentLoaded', () => {
     css.textContent = getGradientCSS() + ";";
